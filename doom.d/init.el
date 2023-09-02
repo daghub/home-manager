125190722ab876b9c06e2e308d37880d2351cf11
Dag Ekengren <dag@ekengren.com>
2023-09-02 22:54:28 +0200

    adds doom emacs nix mode

--- doom.d/init.el
+++ doom.d/init.el
@@ -149,9 +149,9 @@
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       markdown            ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix                 ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
