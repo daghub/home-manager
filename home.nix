1d93d5a0be092c0322dde340e2463b16f8dce555
Dag Ekengren <dag@ekengren.com>
2024-12-11 12:20:22 +0100

    fix zsh config under tmux

--- home.nix
+++ home.nix
@@ -64,7 +64,7 @@
         "pyenv"
       ];
     };
-    initExtra = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme && source .p10k.zsh";
+    initExtra = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme && source ~/.p10k.zsh";
   };
 
   programs.tmux = {
