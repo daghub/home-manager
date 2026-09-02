e34b567e45b49561d97b3b4466e679beec32b416
Dag Ekengren <dekengren@nvidia.com>
2026-09-02 12:47:16 +0300

    Pin Bazel Doom package

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -8,7 +8,8 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
-(package! bazel)
+(package! bazel
+  :pin "27afdb862c2f5266a7f9d621a43f8af75df34712")
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
